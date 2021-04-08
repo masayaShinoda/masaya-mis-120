0b7e3d4c314a45036de455b1664fda3a176eae06
Masaya Shida <masayashida@gmail.com>
2021-04-08 18:47:27 +0700

    major changes

---
+++ typewriter.js
@@ -0,0 +1,32 @@
+//index.html 
+var indexPage = document.getElementById('introductionText');
+var callToAction = document.getElementById('callToAction');
+//lessons.html
+var lessonsPage = document.getElementById('textContainer');
+
+var typewriterIndex = new Typewriter(indexPage, {
+    loop: false
+});
+var typeWriterCallToAction = new Typewriter(callToAction, {
+    loop: false
+});
+var typewriterLessons = new Typewriter(lessonsPage, {
+    loop: false
+});
+typewriterIndex.typeString("Hey there!")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString("My name's Masaya and I'm from section 3 in MIS.")
+    .start();
+
+typeWriterCallToAction.pauseFor(4500).typeString("See MIS lessons ->").start();
+
+typewriterLessons.typeString("Here's what was covered in MIS 120:")
+    // .pauseFor(2500)
+    // .deleteAll()
+    // .typeString('Strings can be removed')
+    // .pauseFor(2500)
+    // .deleteChars(7)
+    // .typeString('<strong>altered!</strong>')
+    // .pauseFor(2500)
+    .start();
